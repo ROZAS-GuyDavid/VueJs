f2c971ea33c8343f3e2a561bc70d4e69f18a9ca9
ROZAS <rozasguydavid@gmail.com>
2019-04-21 20:26:17 +0200

    propriété combinées & watchers 3

--- js/app.js
+++ js/app.js
@@ -2,18 +2,12 @@ let vm = new Vue({
     el: '#app',
     data: {
         firstname: 'jean',
-        lastname:  'Delatour'
+        lastname:  'Delatour',
+        fullname: ''
     },
-    computed: {
-        fullname: {
-            get: function (){
-                return this.firstname + ' ' + this.lastname
-             },
-            set: function (value) {
-                let parts = value.split(' ')
-                this.firstname = parts[0]
-                this.lastname = parts[1]
-            }
+    watch: {
+        fullname: function(value) {
+            console.log('watch', value)
         }
     }
 })
