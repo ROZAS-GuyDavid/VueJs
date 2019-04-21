ccf41d644152b58b4131c736d42c452d09b77ed1
ROZAS <rozasguydavid@gmail.com>
2019-04-21 19:15:06 +0200

    intro

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
-new Vue({
+let vm = new Vue({
     el: '#app',
     data: {
         message : 'hello world',
+        link: 'http:///grafikart.fr',
+        cls: 'alert-success',
+        persons: ['Jonathan','Marion','Marine','Jean','Patrick']
+    },
+    methods: {
+        close: function(){
+            this.success = false
+        }
     }
 })
